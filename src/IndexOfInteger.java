fb76f6a12560205d98acc8c4b327295ae48b6998
404HoneyBadger <94186840+404HoneyBadger@users.noreply.github.com>
2022-05-10 11:32:33 -0400

    second commit

--- src/IndexOfInteger.java
+++ src/IndexOfInteger.java
@@ -12,19 +12,21 @@ public class IndexOfInteger {
 
         System.out.println("Enter a number.");
         
-        Integer fml = scanner.nextInt();
-        userInput.add(fml);    
+        Integer numInput;
+        numInput = scanner.nextInt();// Thiss assigns input to the variable "numInput", the value is saved within the variable
 
-        while (fml != 0){
-            fml = scanner.nextInt();
-            userInput.add(fml);
+        while (numInput != 0){ // this loop adds the 0 because after its entered, 0 is added by "userInput.add(numInput)" THEN the loops checks for 0 and exits.
+            userInput.add(numInput);//Edited to exit with out adding 0 to ArrayList. The add() method then appends what was enetered in line 16
+            numInput = scanner.nextInt();
         }
         System.out.println("Done entering intergers into the list");
         
         System.out.println("What number are you looking forin the list?");
-       Integer why = scanner.nextInt();
-       for (int i = 0; i<userInput.size(); i++ ) {
-           if (why.equals(userInput.get(i))) { System.out.println(userInput.indexOf(why));
+       Integer find = scanner.nextInt();//New variable for input
+       for (int i = 0; i<userInput.size(); i++ ) {// i is the index of the ArrayList "userInput", the loop iterates throught the ArrayList as long as the index is less than the length of ArrayList, incrememnts by 1
+           Integer current = userInput.get(i); // Variable is getting/retrieving the value at the index i, which will change as the loop iterates
+           if (find.equals(current)) { // if user input equals a number in the list at the index being iterated then it prints out
+               System.out.println(find + " is at index " + i);
            }
 
        }
--- src/SumOfList.java
+++ src/SumOfList.java
@@ -6,23 +6,29 @@ public class SumOfList {
         
         Scanner scanner = new Scanner(System.in);
         
-        ArrayList<String> userInput = new ArrayList<>();
+        ArrayList<Integer> userInputArray = new ArrayList<>();
 
-        System.out.println("Enter some text.");
+        System.out.println("Enter some numbers");
         
-        String fml = scanner.next();
-        userInput.add(fml);    
+        Integer userIn = scanner.nextInt();
 
-        while (!fml.equals("0")){
-            fml = scanner.next();
-            userInput.add(fml);
+        while (userIn != 0) {
+            userInputArray.add(userIn);
+            userIn = scanner.nextInt();
         }
-        for ( String userInputs : userInput) {
-            fml += userInputs;
-        }       
-        System.out.print(fml + " were the items in the list. The sum of that list is: " + fml) ;
-
+        
+        Integer sumOfArray = sum(userInputArray);
+        System.out.print(userInputArray + " were the items in the list. The sum of that list is: " +  sumOfArray) ;
     }   
+    public static Integer sum(ArrayList<Integer> list) {
+        Integer sumOfArray = 0; //Variable explicitly starts adding through the iterations at line 22
+        //Enhanced for each loop
+        for ( Integer itemInArrayL : list) { // ":" does the entire for each loop with the default iteration of +1 for ArrayLists
+            sumOfArray += itemInArrayL; 
+        }
+        return sumOfArray;
+    }
+    
 }
    
 
